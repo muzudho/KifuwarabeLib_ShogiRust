732b87b8a114f339b7bc236e5ab3e6b0661c4f2f
muzudho <muzudho1@gmail.com>
2017-12-18 02:54:49 +0900

    ログファイル出力☆（＾～＾）

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
+extern crate lazy_static;
+
 use std::io;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::{BufWriter, Write};
+use std::cell::RefCell;
+
+// グローバル変数の作り方
+thread_local!(
+    // ログ（書き込み・追加モード）
+    pub static LOG: RefCell<BufWriter<File>> ={
+        let m = BufWriter::new(
+            OpenOptions::new().write(true).append(true).open("log.txt").unwrap());
+        RefCell::new(m)
+    }
+);
+
+// 標準出力、ログ出力。末尾に改行を付加
+fn g_writeln( s : &str){
+    println!("{}", s);
+    g_logln(s);
+}
+// ログ出力。末尾に改行を付加
+fn g_logln( s : &str){
+    let bytes = s.as_bytes();
+    // グローバル変数を利用
+    LOG.with(|log| {
+        log.borrow_mut().write(bytes).unwrap();
+        log.borrow_mut().write("\n".as_bytes()).unwrap();
+    })
+}
 
 fn main() {
     loop{
@@ -8,24 +39,27 @@ fn main() {
             .ok()// read_lineの返り値オブジェクトResult の okメソッド
             .expect("info Failed to read line");// OKで無かった場合のエラーメッセージ
 
-        let line : String = line.trim().parse()
-            .ok()
-            .expect("info Failed to parse");
+        // 改行を除く
+        let line : String = line.trim().parse().ok().expect("info Failed to parse");
+        g_logln(&line);
 
         let len = line.chars().count();
 
         // goで始まるコマンドが来たら投了
-        if 1<len && &line[0..2] == "go" { println!("bestmove resign"); break; }
+        if 1<len && &line[0..2] == "go" {
+            g_writeln("bestmove resign");
+            break;
+        }
         // positionで始まるコマンドが来たら無視
         else if 7<len && &line[0..8] == "position" {}
         else {
             match line.as_ref() {
                 "usi" =>{
-                    println!("id name Kifuwarabe_Rust");
-                    println!("id author Muzudho");
-                    println!("usiok")
+                    g_writeln("id name Kifuwarabe_Rust");
+                    g_writeln("id author Muzudho");
+                    g_writeln("usiok")
                 },
-                "isready" => println!("readyok"),
+                "isready" => g_writeln("readyok"),
                 _ =>{},
             }
         }
